96e5d837975389c3cd367b552e79b7fb47e32e4a
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-09-22 17:23:31 -0300

    prints

--- 2025-09-22/exemplo01.py
+++ 2025-09-22/exemplo01.py
@@ -1,1 +1,33 @@
-intValor = 63
+import sys
+
+try:
+    intValor = int(input('Digite um numero inteiro: '))
+except ValueError:
+    sys.exit('Valor inválido')
+except KeyboardInterrupt:
+    sys.exit('\nPrograma interrompido pelo usuário.')
+except Exception as erro:
+    sys.exit(f'Erro: {erro}.')
+else:
+    '''print(f'{intValor} em Binário...: {bin(intValor):08b}')
+
+    print(f'{intValor} em Hexadecimal...: {hex(intValor)}')
+
+    print(f'{intValor} em Octal...: {oct(intValor)}')'''
+
+    if intValor < 0:
+        sys.exit('Por favor, insira um numero inteiro não negativo.')
+    
+    intQuantBits = intValor.bit_length()
+
+    # converter pra bin e remover '0b'
+    binValor = bin(intValor)[2:]
+
+    if len(binValor) % 8 != 0:
+        # quantos zeros sao necessarios pra completar o byte
+        intZeros = 8 - len(binValor) % 8
+
+    # add os zeros a esquerda
+    binValorFinal = ('0' * intZeros) + binValor
+
+    print(f'\n{intValor} em Binário...: 0b{binValorFinal}')
