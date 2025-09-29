44c25459f5c16fe8983169b2aaf0aee0c9b6766b
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-09-29 17:36:11 -0300

    codigos do prof

--- 2025-09-24 (op bin com ip)/exemplo02.py
+++ 2025-09-24 (op bin com ip)/exemplo02.py
@@ -1,8 +1,25 @@
+'''
+   Exemplo 02 - Calculando a máscara de sub-rede em formato inteiro 
+   com base em um valor de CIDR (Classless Inter-Domain Routing)
+'''
+intCIDR = 24
+print(f'Valor CIDR (bits na máscara)..............: /{intCIDR}\n')
+
+intMascara = 0xFFFFFFFF >> (32 - intCIDR)
+
+intMascara = intMascara << (32 - intCIDR)
+
+print(f'Máscara de sub-rede como inteiro..........: {intMascara}\n')
+
+print(f'Máscara de sub-rede em binário é (32 bits): {intMascara:032b}\n')
+
+#
+
 '''intCIDR = 24
 
 intMascara = 0xFFFFFFFF >> (32 - intCIDR)
 
-intMascara = intMascara << (32 - intCIDR)'''
+intMascara = intMascara << (32 - intCIDR)
 
 # Será impresso -> 4294967040 (4.294.967.040)
 
@@ -12,4 +29,4 @@ intCIDR = 24
 intIP      = int.from_bytes(bytes([int(x) for x in strIP.split('.')]) ,'big')
 
 intMascara = 0xFFFFFFFF >> (32 - intCIDR) << (32 - intCIDR)
-print(intMascara)
+print(intMascara)'''
---
+++ 2025-09-24 (op bin com ip)/exemplo03.py
@@ -0,0 +1,20 @@
+'''
+   Exemplo 03 -  Calculando do endereço de rede a partir de um IP e CIDR
+'''
+strIP   = '192.168.1.10'
+print(f'O Endereço IPv4 é...........................: {strIP}\n')
+
+intCIDR = 24
+print(f'Valor CIDR (bits na máscara)................: /{intCIDR}\n')
+
+intIP      = int.from_bytes(bytes([int(x) for x in strIP.split('.')]), 'big')
+print(f'O Endereço IPv4 em binário é (32 bits)......: {intIP:032b}\n')  
+
+intMascara = 0xFFFFFFFF >> (32 - intCIDR) << (32 - intCIDR)
+print(f'Máscara de sub-rede em binário é (32 bits)..: {intMascara:032b}\n')  
+
+intIPRede  = intIP & intMascara
+print(f'Endereço IPv4 da Rede em binário é (32 bits): {intIPRede:032b}\n')  
+
+strIPRede = '.'.join([str(x) for x in intIPRede.to_bytes(4)])
+print(f'O Endereço IPv4 da Rede é...................: {strIPRede}\n')
