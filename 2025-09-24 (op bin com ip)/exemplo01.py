7e16bab30a482fe75bc0460e129f82f832b3d893
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-09-29 16:56:11 -0300

    att codigos

--- 2025-09-24 (op bin com ip)/exemplo01.py
+++ 2025-09-24 (op bin com ip)/exemplo01.py
@@ -1,4 +1,26 @@
-strIP = '255.255.255.0'
+'''
+   Exemplo 01 - Convertendo um endereço IP em inteiro de 32 bits
+'''
+strIP = '192.168.1.10'
+print(f'O Endereço IPv4 é.....................: {strIP}\n')
+
+lstIP = [int(x) for x in strIP.split(".")]
+
+print(f'Lista de inteiros (octetos)...........: {lstIP}\n')
+
+bytesIP = bytes(lstIP)
+
+print(f'Endereço IPv4 como bytes..............: {bytesIP}\n')
+
+intIP = int.from_bytes(bytes(lstIP), byteorder='big')
+
+print(f'Endereço IPv4 como inteiro ...........: {intIP}\n')
+
+print(f'O Endereço IPv4 em binário é (32 bits): {intIP:032b}\n')  
+
+#
+
+'''strIP = '255.255.255.0'
 
 # --------------------------------------------------
 # Gerando uma lista com 4 posições
@@ -20,4 +42,4 @@ print(binIP)
 # converter pra inteiro
 intIP = int.from_bytes(bytes(lstIP), byteorder="big")
 
-print(f'int IP...: {intIP}')
+print(f'int IP...: {intIP}')'''
