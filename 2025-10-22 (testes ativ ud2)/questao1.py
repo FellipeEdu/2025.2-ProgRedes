030ca1744f66fda45c2f3369b32307abc9d28a7f
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-10-22 17:50:01 -0300

    testando digest

--- 2025-10-22 (testes ativ ud2)/questao1.py
+++ 2025-10-22 (testes ativ ud2)/questao1.py
@@ -1,20 +1,46 @@
 import hashlib; from time import time
 
-strBinTestes = 'É possivel calcular essa?'.encode()
-intDificuldade = 18
+def findNonce(dataToHash: bytes, bitsZero: int):
+    while True:
+        bytesInput = nonce.to_bytes(4, 'big') + dataToHash
+
+        target = 1 << (256 - bitsZero)
+        hashDigest = hashlib.sha256(bytesInput).digest()
+        intHash = int.from_bytes(hashDigest, 'big')
+
+        if intHash < target:
+            tempoDec = time() - inicio
+            print(f'HASH: {hashDigest}')
+            print(f'NONCE: {nonce} TEMPO: {tempoDec:.4f} s')
+            break   
+        nonce += 1
+
+#strBinTestes = 'É possivel calcular essa?'.encode()
+lstEntrada = [['Esse é facil', 8], ['Esse é facil', 10], ['Esse é facil', 15],
+              ['Texto maior muda o tempo?', 8], ['Texto maior muda o tempo?', 10], ['Texto maior muda o tempo?', 15],
+              ['É possível calcular esse?', 18], ['É possível calcular esse?', 19], ['É possível calcular esse?', 20]]
+#intDificuldade = 18
 nonce = 0
 inicio = time()
 
-while True:
-    bytesInput = nonce.to_bytes(4, 'big') + strBinTestes
+#while True:
+for texto in lstEntrada:
+    while True:
+        bytesInput = nonce.to_bytes(4, 'big') + texto[0].encode()
 
-    #hashResultado = hashlib.sha256(bytesInput).hexdigest()
-    #if hashResultado.startswith('0' * ((intDificuldade // 4) + (intDificuldade % 4))): break
+        #hashResultado = hashlib.sha256(bytesInput).hexdigest()
+        #if hashResultado.startswith('0' * ((intDificuldade // 4) + (intDificuldade % 4))): break
+        target = 1 << (256 - texto[1])
+        hashDigest = hashlib.sha256(bytesInput).digest()
+        intHash = int.from_bytes(hashDigest, 'big')
 
-    hashResultado = hashlib.sha256(bytesInput).digest()
-        
-    nonce += 1
-        
-tempoDecorrido = time() - inicio
+        if intHash < target:
+            tempoDec = time() - inicio
+            print(f'HASH: {hashDigest}')
+            print(f'NONCE: {nonce} TEMPO: {tempoDec:.4f} s')
+            break   
+        nonce += 1
 
-print(f'HASH: {hashResultado} NONCE: {nonce} TEMPO: {tempoDecorrido:.4f} s')
+# tempoDecorrido = time() - inicio
+
+#print(f'NONCE: {nonce} TEMPO: {tempoDecorrido:.4f} s')
