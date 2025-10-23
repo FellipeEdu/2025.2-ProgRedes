d2599d769aa9c8f0c485b0e8266d9a8ab8722703
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-10-23 11:25:17 -0300

    atualizando

--- 2025-10-22 (testes ativ ud2)/questao1.py
+++ 2025-10-22 (testes ativ ud2)/questao1.py
@@ -1,6 +1,8 @@
 import hashlib; from time import time
 
 def findNonce(dataToHash: bytes, bitsZero: int):
+    inicio = time()
+    nonce = 0
     while True:
         bytesInput = nonce.to_bytes(4, 'big') + dataToHash
 
@@ -10,10 +12,22 @@ def findNonce(dataToHash: bytes, bitsZero: int):
 
         if intHash < target:
             tempoDec = time() - inicio
-            #print(f'HASH: {hashDigest}')
-            #print(f'NONCE: {nonce} TEMPO: {tempoDec:.4f} s')
-            return nonce, tempoDec   
+
         nonce += 1
+        return nonce, tempoDec   
+
+def findNonceHex(dataToHash: bytes, bitsZero: int):
+    inicio = time()
+    nonce = 0
+    while True:
+        bytesInput = nonce.to_bytes(4, 'big') + dataToHash
+
+        hashDigest = hashlib.sha256(bytesInput).hexdigest()
+        if hashDigest.startswith('0' * ((bitsZero // 4) + (bitsZero % 4))):
+            tempoDec = time() - inicio 
+            return nonce, tempoDec
+             
+        nonce += 1           
 
 #strBinTestes = 'É possivel calcular essa?'.encode()
 lstEntrada = [['Esse é facil', 8], ['Esse é facil', 10], ['Esse é facil', 15],
@@ -21,10 +35,13 @@ lstEntrada = [['Esse é facil', 8], ['Esse é facil', 10], ['Esse é facil', 15]
               ['É possível calcular esse?', 18], ['É possível calcular esse?', 19], ['É possível calcular esse?', 20]]
 #intDificuldade = 18
 nonce = 0
-inicio = time()
 
 #while True:
 for texto in lstEntrada:
-    nonceEncontrado, tempoDecorrido = findNonce(texto[0], texto[1])
+    nonceEncontrado, tempoDecorrido = findNonce(texto[0].encode(), texto[1])
+    nonce2, tempo2 = findNonceHex(texto[0].encode(), texto[1])
+
     print(f'NONCE: {nonceEncontrado} TEMPO: {tempoDecorrido:.4f} s')
+    print(f'NONCE: {nonce2} TEMPO: {tempo2:.4f} s')
+    
 # COMPARAR FUNCOES COM DIGEST E HEXDIGEST
