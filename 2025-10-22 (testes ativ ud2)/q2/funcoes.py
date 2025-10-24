5946bc8f144d76921bc734e3d9aef4e5691fa261
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-10-23 22:24:06 -0300

    desenvolvendo inicializaRAID()

--- 2025-10-22 (testes ativ ud2)/q2/funcoes.py
+++ 2025-10-22 (testes ativ ud2)/q2/funcoes.py
@@ -1,14 +1,17 @@
 import os
 
-diretorio = os.path.dirname(__file__)
-
 def inicializaRAID():
     quantDiscos = int(input('Quantidade de discos: '))
     tamanhoDiscos = int(input('Tamanho de cada disco (em bytes): '))
     tamanhoBlocos = int(input('Tamanho de blocos dos discos (em bytes): '))
+    pasta = input('Nome da pasta dos arquivos: ')
 
-    for disco in range(quantDiscos):
-        open(f'{diretorio}\\disco{disco}', '+ab')
+    diretorio = os.makedirs(pasta, exist_ok=True)
+
+    for disco in range(quantDiscos - 1):
+        open(f'{diretorio}\\disco{disco}.bin', '+ab')
+
+
     
 def obtemRAID()
     
