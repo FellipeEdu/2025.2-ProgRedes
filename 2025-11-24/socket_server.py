0680962ca3164c6fcd2c717a968efe327c241758
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-11-24 17:52:50 -0300

    mensagens

--- 2025-11-24/socket_server.py
+++ 2025-11-24/socket_server.py
@@ -15,23 +15,30 @@ sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Ligando o socket à porta
 sockServer.bind( (HOST_IP_SERVER, HOST_PORT) )
 
-print('\nRecebendo Mensagens...\n\n')
+# definindo tempo de vida
+sockServer.settimeout(0.5)
+
+print('\nRecebendo Mensagens...\n')
 print('Pressione CTRL+C para sair do Servidor.\n')
-print('-' * 50)
+print(f'MENSAGENS ABAIXO\n{'-' * 50}')
 
 try:
     while True:
-        # Recebendo os dados do cliente
-        byteMensagem, tuplaCliente,  = sockServer.recvfrom(BUFFER_SIZE)
+        try:
+             # Recebendo os dados do cliente
+            byteMensagem, tuplaCliente,  = sockServer.recvfrom(BUFFER_SIZE)
+        except socket.timeout:
+            continue
+        else:
+            strNomeHost = socket.gethostbyaddr(tuplaCliente[0])[0]
+            strHost = strNomeHost.split('.')[0].upper()
 
-        strNomeHost = socket.gethostbyaddr(tuplaCliente[0])[0]
-        strHost = strNomeHost.split('.')[0].upper()
-
-        # Imprimindo a mensagem recebida convertendo de bytes para string
-        print(f'{tuplaCliente} -> {strHost}: {byteMensagem.decode(CODE_PAGE)}')
+            # Imprimindo a mensagem recebida convertendo de bytes para string
+            print(f'{tuplaCliente} -> {strHost}: {byteMensagem.decode(CODE_PAGE)}')
+            
 except KeyboardInterrupt:
-    print('\nAVISO: foi preccionado CTRL+C.\nSaindo do servidor...\n\n')
+    print('\nAVISO: foi pressionado CTRL+C.\nSaindo do servidor...\n')
 finally:
     # Fechando o socket
     sockServer.close()
-    print('Servidor finalizado com sucesso.\n\n')
+    print('Servidor finalizado com sucesso.\n')
