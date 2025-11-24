88301931eaafd74c05d05cf80f4f15c47fd0776a
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-11-24 17:43:47 -0300

    try except

--- 2025-11-24/socket_server.py
+++ 2025-11-24/socket_server.py
@@ -13,16 +13,25 @@ CODE_PAGE       = 'utf-8'         # Definindo a página de
 sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Ligando o socket à porta
-sockServer.bind( (HOST_IP_SERVER, HOST_PORT) ) 
+sockServer.bind( (HOST_IP_SERVER, HOST_PORT) )
 
 print('\nRecebendo Mensagens...\n\n')
+print('Pressione CTRL+C para sair do Servidor.\n')
+print('-' * 50)
 
-while True:
-    # Recebendo os dados do cliente
-    byteMensagem, tuplaCliente,  = sockServer.recvfrom(BUFFER_SIZE)
+try:
+    while True:
+        # Recebendo os dados do cliente
+        byteMensagem, tuplaCliente,  = sockServer.recvfrom(BUFFER_SIZE)
 
-    strNomeHost = socket.gethostbyaddr(tuplaCliente[0])[0]
-    strHost = strNomeHost.split('.')[0].upper()
-    
-    # Imprimindo a mensagem recebida convertendo de bytes para string
-    print(f'{tuplaCliente} -> {strHost}: {byteMensagem.decode(CODE_PAGE)}')
+        strNomeHost = socket.gethostbyaddr(tuplaCliente[0])[0]
+        strHost = strNomeHost.split('.')[0].upper()
+
+        # Imprimindo a mensagem recebida convertendo de bytes para string
+        print(f'{tuplaCliente} -> {strHost}: {byteMensagem.decode(CODE_PAGE)}')
+except KeyboardInterrupt:
+    print('\nAVISO: foi preccionado CTRL+C.\nSaindo do servidor...\n\n')
+finally:
+    # Fechando o socket
+    sockServer.close()
+    print('Servidor finalizado com sucesso.\n\n')
