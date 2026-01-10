4d7fcdb064f241729f808b5474c5ff93c93bd1d4
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2026-01-10 16:44:13 -0300

    menu

--- 2026-01-01 (testes ativ ud4)/client_file_tcp.py
+++ 2026-01-01 (testes ativ ud4)/client_file_tcp.py
@@ -14,77 +14,23 @@ if not server_host:
 dir_Existe(DIR_IMG_CLIENT)
 
 while True:
-    nome = input('Digite o arquivo para receber: ').strip()
-    if not nome:
-        continue
-    if nome.lower() == 'sair':
-        break
-    # passa server_host como primeiro parâmetro explicitamente
-    solicitar_Arq(HOST_IP_SERVER, nome, DIR_IMG_CLIENT)
+   print(f"\n{'=' * 10} Menu {'=' * 10}")
+   print("1. Solicitar Arquivo")
+   print("2. tbd")
+   print("3. tbd")
+        
+   escolha = input("Escolha uma opção: ")
+        
+   if escolha == '1':
+      nome = input('Digite o arquivo para receber: ').strip()
+      if not nome:
+         continue
+      if nome.lower() == 'sair':
+         break
 
-'''# Limpando a tela do terminal
-os.system('cls') if os.name == 'nt' else os.system('clear')
+      solicitar_Arq(HOST_IP_SERVER, nome, DIR_IMG_CLIENT)
 
-try:
-   # Criando o socket
-   sockClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-   
-   # Definindo o timeout para o socket do cliente
-   sockClient.settimeout(constantes.TIMEOUT_SOCKET)
-
-   # Enviando uma 'sinalização' de que um cliente se conectou ao servidor (FRAGMENTADO)
-   strMensagemEntrada = f'{chr(175)} {constantes.HOST_IP_CLIENT}'
-   byteMensagemEntrada = strMensagemEntrada.encode(constantes.CODE_PAGE)
-
-   # Mensagem inicial do cliente
-   print('\n' + '-'*100)
-   print('CLIENTE UDP Inicializado - Enviando Comandos...')
-   print('Digite SAIR para sair do cliente...\n')
-   print(f'IP/Porta do Cliente.:{constantes.TUPLA_CLIENTE}')
-   print('-'*100 + '\n')
-
-   # Loop principal do cliente
-   while True:
-      strNomeArquivo = input('Digite o arquivo para receber: ').lower().strip()
-
-      # Se teclar ENTER sem digitar nada, ignora
-      if not strNomeArquivo: continue
-
-      if strNomeArquivo == 'sair': break
-
-      # Dizendo ao servidor que deseja receber um arquivo
-      byteMensagem = strNomeArquivo.encode(constantes.CODE_PAGE)
-
-      # Enviando a mensagem ao servidor
-      sockClient.sendto(byteMensagem, constantes.TUPLA_SERVER)
-
-      # Preparando para receber o arquivo
-      objArquivoRecebimento = open(f'{constantes.DIR_IMG_CLIENT}\\{strNomeArquivo}', 'wb')
-      print(f'\nRecebendo o arquivo "{strNomeArquivo}" do Servidor...')
-
-      while True:
-         # Recebendo os dados do servidor
-         byteDados, tuplaServidor = sockClient.recvfrom(constantes.BUFFER_SIZE)
-    
-         # Se a mensagem recebida for 'EOF', significa que o arquivo terminou
-         if byteDados == b'EOF':
-            print(f'Arquivo "{strNomeArquivo}" recebido com sucesso!\n')
-            objArquivoRecebimento.close()
-            break
-    
-         # Escrevendo os dados recebidos no arquivo
-         objArquivoRecebimento.write(byteDados)
-
-      # Fechando o arquivo após o recebimento
-      objArquivoRecebimento.close()
-except KeyboardInterrupt:
-   print('\nAVISO.........: Foi Pressionado CTRL+C...\nSaindo do Cliente...\n\n')
-except socket.error as strErro:
-   print(f'\nERRO DE SOCKET: {strErro}\n\n')
-except Exception as strErro:
-   print(f'\nERRO GENÉRICO..: {strErro}\n\n')
-finally:
-   sockClient.close()
-   print('Cliente finalizado com Sucesso...\n\n')'''
-
-#-------------------------------
+   '''elif escolha == '2':
+      funcoes.obtemRAID()
+   elif escolha == '3':
+      funcoes.escreveRAID()'''
