2c3d49523585dac46ab0ccae004fb25ae5cb7f7c
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2026-01-21 17:53:24 -0300

    cod novos

---
+++ 2026-01-19 (telegram bot)/bot_v3.py
@@ -0,0 +1,58 @@
+import os, sys, requests, platform
+
+'''
+    Deve ser criado um arquivo com o nome token_bot.py
+    e dentro criar a "constante" API_TOKEN e
+    atribuir o valor do token informado pelo Bot Father
+'''
+
+from token_bot import *
+
+strURLBase        = f'https://api.telegram.org/bot{API_TOKEN}'
+strURLGetUpdates  = f'{strURLBase}/getUpdates'
+strURLSendMessage = f'{strURLBase}/sendMessage'
+
+# Limpa a tela
+os.system('cls' if platform.system() == 'Windows' else 'clear')
+
+# Mensagem inicial
+print('\nBOT TELEGRAM - Aguardando mensagens...')
+print('---------------------------------------\n')
+
+# Inicializa a variável de controle do ID da última mensagem
+# -1 indica que nenhuma mensagem foi recebida ainda
+intIDUltimaMensagem = -1
+
+# Loop infinito -> Modo Passivo
+while True:
+   # Obtém as mensagens
+   reqURL = requests.get(strURLGetUpdates)
+
+   # Verifica se a requisição não foi bem sucedida
+   if not reqURL.status_code == 200:
+      sys.exit('\nERRO: Erro ao acessar a URL\nCÓDIGO DE RETORNO: ' + str(reqURL.status_code))
+
+   # Verifica se não há mensagens
+   if reqURL.json()['result'] == []: continue
+
+   # Converte a resposta para JSON
+   jsonRetorno = reqURL.json()
+
+   # Obtém o ID da última mensagem da Requisição
+   intIDMensagemAtual = jsonRetorno["result"][-1]["message"]["message_id"]
+
+   # Verifica se o ID da última mensagem é igual ao ID da mensagem atual
+   # Se for igual, volta o loop para o início (ignora a mensagem)
+   if intIDMensagemAtual == intIDUltimaMensagem: continue
+      
+   # Atualiza o ID da última mensagem
+   intIDUltimaMensagem = intIDMensagemAtual
+   
+   strMensagem = jsonRetorno["result"][-1]["message"]["text"]
+   print(f'Mensagem recebida {intIDUltimaMensagem}: {strMensagem}')
+   
+   # Envia a mensagem de retorno
+   intIDChat            = jsonRetorno["result"][-1]["message"]["chat"]["id"]
+   strMensagemDevolvida = f'Você enviou a mensagem:\n"{strMensagem.upper()}"'
+   dictRetorno          = {'chat_id':intIDChat, 'text':strMensagemDevolvida}
+   reqURL = requests.post(strURLSendMessage, data=dictRetorno) 
