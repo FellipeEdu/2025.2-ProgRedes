cbf640744941ac1dfe533cd6271589d61d519c43
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2026-02-07 15:00:59 -0300

    mostrarAgentes()

--- 2026-01-27 (testes ativ final)/funcoes_bot.py
+++ 2026-01-27 (testes ativ final)/funcoes_bot.py
@@ -38,11 +38,19 @@ def thread_aguardar_agentes(dictAgentes):
 
 # ----------------------------------------------------------------------
 def startBot() -> str:
-   """
-      Retorna mensagem de boas vindas do bot.
-   """
-   return (
-      "Bem-vindo ao BOT SHOW METRICS CNAT!\n\n"
-      "Este é um bot para consultar métricas de performance dos dispositivos conectados.\n\n"
-      "/? → Exibe mensagem de ajuda."
+    """
+    Retorna mensagem de boas vindas do bot.
+    """
+    return (
+        "Bem-vindo ao BOT SHOW METRICS CNAT!\n\n"
+        "Este é um bot para consultar métricas de performance dos dispositivos conectados.\n\n"
+        "/? → Exibe mensagem de ajuda."
+        )
+
+def mostrarAgentes(dictAgentes) -> str:
+    """
+    Retorna os agentes conectados.
+    """
+    return (
+        "Agentes conectados:\n" + ("\n".join(dictAgentes.keys()) if dictAgentes else "Nenhum")
     )
